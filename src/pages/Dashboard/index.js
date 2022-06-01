a44321af92f3e55c3917f424a64b6186cde135ef
HuyenTrang <trangnguyen9b2013@gmail.com>
2022-06-02 01:17:36 +0700

    Thêm calendar vào dashboard và events

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,19 +2,28 @@ import { faUsers } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import classNames from "classnames/bind";
 import styles from "./Dashboard.module.css";
+// import calendar
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const cx = classNames.bind(styles);
 
 function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const onChange = (date) => {
+    setDate(date);
+  };
   return (
-    <div className={cx("wrapper")}>
+    <div className={cx('wrapper')}>
       {/* Tiêu đề trang chủ */}
       <h1 className={cx("page-name")}>Trang chủ</h1>
 
       {/* Nội dung tổng hợp đầu trang chủ */}
-      <div className={cx("row")}>
+      <div className={cx('row')}>
         <div className={cx("col-4")}>
-          <a href="#">
+          <a href="/">
             <div className={cx("elements")}>
               <div className={cx("block-title")}>
                 <h5 className={cx("title")}>Số lượng khách hàng</h5>
@@ -30,7 +39,7 @@ function Dashboard() {
         </div>
 
         <div className={cx("col-4")}>
-          <a href="#">
+          <a href="/">
             <div className={cx("elements")}>
               <div className={cx("block-title")}>
                 <h5 className={cx("title")}>Số lượng nhân viên</h5>
@@ -46,7 +55,7 @@ function Dashboard() {
         </div>
 
         <div className={cx("col-4")}>
-          <a href="#">
+          <a href="/">
             <div className={cx("elements")}>
               <div className={cx("block-title")}>
                 <h5 className={cx("title")}>Số lượng công việc</h5>
@@ -61,7 +70,16 @@ function Dashboard() {
           </a>
         </div>
       </div>
-       {/* Code lịch làm việc */}
+
+      {/* Code lịch làm việc */}
+      <div className={cx('row calendars')}>
+        {/* Code lịch bình thường - bên trái */}
+        <div className={cx('col-4 calendarLeft')}>
+          <Calendar onChange={onChange} value={date} />
+        </div>
+        {/* Code thông báo nhắc việc hôm nay - bên phải */}
+        <div className={cx('col-8 scheduleRight')}></div>
+      </div>
     </div>
   );
 }
--- src/pages/Events/index.js
+++ src/pages/Events/index.js
@@ -1,14 +1,32 @@
-import classNames from 'classnames/bind';
-import styles from './Events.module.css';
+import classNames from "classnames/bind";
+import styles from "./Events.module.css";
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+// import interactionPlugin from "@fullcalendar/interaction";
 
-const cx = classNames.bind(styles)
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
 
 function Events() {
-    return (
-        <div className={cx('wrapper')}>
-            <h1 className={cx('page-name')}>Cuộc hẹn</h1>
+  return (
+    <div className={cx("wrapper")}>
+      <h1 className={cx("page-name")}>Lịch làm việc</h1>
+
+        {/*code lịch làm việc - events bằng Fullcalendar*/}
+      <div className={cx("table-actions row")}>
+        <div className={cx("col-6")}>
+          <button className={cx("add-btn")}>
+            <FontAwesomeIcon className={cx("plus-icon")} icon={faPlus} />
+            Thêm sự kiện
+          </button>
         </div>
-    )
+      </div>
+
+      <div className={cx('calendar')}><Fullcalendar plugins={[dayGridPlugin]} /></div>
+    </div>
+  );
 }
 
 export default Events;
